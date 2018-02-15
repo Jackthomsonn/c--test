21afbf3698edc00e74f429248e9082c385d0c679
Jack Thomson <hello@jackthomson.co.uk>
2018-02-15 19:23:29 +0000

    Initial Commit

---
+++ Test-Project/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestProject
+{
+    public class Book
+    {
+        private readonly List<string> books = new List<string>();
+
+        public void AddBook(string book)
+        {
+            books.Add(book);
+        }
+
+        public void RemoveBook(string name)
+        {
+            int index = books.FindIndex(bookName => bookName == name);
+
+            books.RemoveAt(index);
+        }
+
+        public List<string> GetBooks() => books;
+    }
+}
---
+++ Test-Project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Book book = new Book();
+
+            book.AddBook("My First Book");
+
+            Console.WriteLine("New List Of Books");
+
+            book.GetBooks().ForEach(Console.WriteLine);
+
+            Thread.Sleep(3000);
+
+            book.AddBook("My Second Book");
+
+            Console.WriteLine("New List Of Books");
+
+            book.GetBooks().ForEach(Console.WriteLine);
+
+            Thread.Sleep(3000);
+
+            book.RemoveBook("My First Book");
+
+            Console.WriteLine("Should remove first book..");
+
+            Console.WriteLine("New List Of Books");
+
+            book.GetBooks().ForEach(Console.WriteLine);
+
+        }
+    }
+}
